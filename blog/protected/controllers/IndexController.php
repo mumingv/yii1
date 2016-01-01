cbf9b56c26912a189846a3b90c437eb261d8b3ab
Jay Yin <mumingv@163.com>
2016-01-01 19:42:28 +0800

    Using render method

--- blog/protected/controllers/IndexController.php
+++ blog/protected/controllers/IndexController.php
@@ -11,6 +11,22 @@ class IndexController extends Controller {
 	 * http://localhost/index.php?r=index
 	 */
 	public function actionIndex() {
-		echo "www.baidu.com";
+		/**
+		 * 1. render方法会载入默认布局
+		 * 默认布局在components/Controller.php中进行设置
+		 * 默认值为：$layout='//layouts/column1'
+		 * 2. 这里的参数'index'，
+		 * 对应的是views/index/index.php文件而不是index目录
+		 * 注：index目录对应控制器名称
+		 */
+		$this->render('index');
+		/**
+		 * renderPartial方法不会载入默认布局，也不会载入框架自带的jQuery		 
+		 */
+		// $this->renderPartial('index');	
+	}
+
+	public function actionAdd() {
+		$this->render('add');
 	}
 }
